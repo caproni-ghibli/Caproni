30faef0d29759d3ddbb726c427ad18dde54cf5e4
zacksailor <zakarabenjawed@gmail.com>
2024-11-19 01:08:18 +0100

    fix layout

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -10,53 +10,62 @@ interface Params {
 function WorkPage({ params }: { params: Params }) {
   const skill = PAST_ROLES[params.id];
   return (
-    <div>
+    <div className='w-full place-items-center'>
 
-      <h2 className="text-xl font-heading sm:text-2xl">
-        {skill.role}
-      </h2>
+      <div className=' px-5 w-[750px] max-w-full'>
 
-      <div className='flex items-center gap-1'>
+        <h2 className="text-xl font-heading sm:text-2xl">
+          {skill.role}
+        </h2>
 
-        <FontAwesomeIcon className='h-3' icon={faCalendar} />
+        <div className='flex items-center gap-1'>
+
+          <FontAwesomeIcon className='h-3' icon={faCalendar} />
+          
+          <span>{skill.startDate}</span>
+          
+          {!!skill.endDate && "-"}
+          
+          <span>{skill.endDate}</span>
+
+        </div>
+
+        <p className="mt-2 mr-auto">{skill.company}</p>
+
+        <br />
+                    
+        <p>{skill.description}</p>
         
-        <span>{skill.startDate}</span>
+        <br />
         
-        {!!skill.endDate && "-"}
-        
-        <span>{skill.endDate}</span>
-
       </div>
 
-      <p className="mt-2 mr-auto">{skill.company}</p>
-
-      <br />
-                  
-      <p>{skill.description}</p>
-      
-      <br />
-      
-      <div className='w-[calc(100dvw-16px)] -ml-[calc(50dvw-375px+15px)]'>
+      <div className='w-full'>
       
         {!!skill.skills && <Marquee items={ skill.skills } />}
       
       </div>
 
-      <br />
 
-      <ul className='list-disc'>
-        {skill.keyResponsibilities.map((keyResponsibility, id) => (
-          <li className='m-3' key={id}>{keyResponsibility}</li>
-        ))}
-      </ul>
+      <div className='px-5 w-[750px] max-w-full'>
 
-      <br />
+        <br />
 
-      <p>
-         {!!skill.closingStatement && skill.closingStatement}
-      </p>
+        <ul className='list-disc'>
+          {skill.keyResponsibilities.map((keyResponsibility, id) => (
+            <li className='m-3' key={id}>{keyResponsibility}</li>
+          ))}
+        </ul>
 
+        <br />
+
+        <p>
+          {!!skill.closingStatement && skill.closingStatement}
+        </p>
+
+      </div>
     </div>
+  
   );
 }
 
